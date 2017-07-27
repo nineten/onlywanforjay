83cab3d099522f0c01821c8f43fd1a2fa4358fba
jaydencodigo <jayden@codigo.sg>
2017-07-27 17:24:31 +0800

    feat: index now has a parallax header

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,25 @@
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    var parallax = document.querySelectorAll(".parallax");
+    var viewheight = window.innerHeight;
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+        elBackgrounPos = "50% -" + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+      var elc = el.children;
+      Array.from(elc).forEach(function(el, i) {
+        var newOpacity = 1.0 - (windowYOffset / viewheight * 2);
+        el.style.opacity = newOpacity
+      });
+
+    });
+  };
+
+})();
