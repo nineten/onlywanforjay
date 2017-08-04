6d1abdc03907d7046c7b0112283cb118627fc242
jaydencodigo <jayden@codigo.sg>
2017-08-04 16:58:44 +0800

    feat: no_of_guests now generates additional fields for guest names

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -26,4 +26,25 @@
 
 $(document).ready(function() {
   $('.labelholder').labelholder();
+
+  $('#rsvp_no_of_guests').change(function(e) {
+    var value = $(this).val();
+    console.log(value);
+    console.log(value > 1);
+
+    if (value > 1) {
+      $("#additional-guests").show();
+      $("#additional-guests-names").html("");
+
+      for (i = 1; i < value; i++) {
+        var input_field = "<div class='form-group labelholder' data-label='ADDITIONAL GUEST NAME " + i + "'><input placeholder='Additional guest name " + i + "' class='form-control input-lg' type='text' name='rsvp[names_of_additional_guests][]' id='rsvp_names_of_additional_guests[" + i + "]'></div>";
+        $("#additional-guests-names").append(input_field);
+      }
+
+      $("#additional-guests-names > .labelholder").labelholder();
+      $("#rsvp_no_of_baby_chairs_needed").attr({max: value - 1});
+    } else {
+      $("#additional-guests").hide();
+    }
+  });
 });
